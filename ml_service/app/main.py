fcbbc5eac5f9025f2ec991a64fc58bc9f9a65365
tamadevu <tori.amadevu@gmail.com>
2024-07-28 13:11:07 +0100

    fix: linting

--- ml_service/app/main.py
+++ ml_service/app/main.py
@@ -4,6 +4,22 @@ from fastapi import FastAPI, HTTPException
 from uuid import uuid4
 import random
 from ml_service.schemas.dataset import CreateDatasetSchema, DataSchema
+
+app = FastAPI()
+
+
+@app.get("/health")
+async def health():
+    """
+    A function that returns the health status of the application.
+
+    Returns:
+        dict: A dictionary containing the status of the application. The status is a string indicating that the application is running at a specific port.
+
+    """
+    return {"status": "running"}
+
+
 @app.post("/dataset")
 async def dataset(body: CreateDatasetSchema):
     """
